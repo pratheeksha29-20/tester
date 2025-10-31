3debe27bd5d767b687a0c9c645ae8ab32fcc0fea
pratheeksha29-20 <pratheekshadevaraj.phd.123@gmail.com>
2025-10-31 13:12:24 +0530

    added name , age , phone number

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function App() {
-  return (
-    <div>App</div>
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [number,setNumber]= useState('');
+  const handlesubmit =(e) =>
+{
+  e.preventDefault()
+  {console.log(name)}
+{console.log(age)}
+{console.log(number)}
+
+} 
+ return (
+    <div>
+      <form onSubmit ={handlesubmit}>
+        <input type="text" placeholder="Enter your name" value = { name}
+            onChange={(e) => setName(e.target.value)} />
+        <input type="number" placeholder="Enter your age" value = {age}
+            onChange={(e) => setAge(e.target.value)}/>
+        <input type ="number" placeholder="enter your number" value = {number}
+            onChange={(e) => setNumber(e.target.value)}/>     
+           <button type='submit'>submit</button>
+
+      </form>
+      <p>your name is</p>
+       <p> your age is</p>
+       <p> your number is</p>
+       <span>
+      
+       </span>
+
+
+    </div>
   )
 }
 
